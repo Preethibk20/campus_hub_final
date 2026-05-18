8e46692931ea42f58b6278af5b1d3e98c2057d6c
Preethibk20 <preethibkiran@gmail.com>
2026-05-18 18:40:09 +0530

    cleanup: remove stale root prototype folders, old auth server, and debug logs

--- App.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import SimplifiedApp from './components/SimplifiedApp';
-
-const App: React.FC = () => {
-  return <SimplifiedApp />;
-};
-
-export default App;
--- start-auth.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Campus Hub Auth Server (Node.js)...
-cd auth-server
-npm install && node server.js
-pause
